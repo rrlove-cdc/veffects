7622d0cf03189a33e3f45f67aa47b10f381b9f38
Becca Love <rlove1@nd.edu>
2018-05-05 18:05:07 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# veffects
+Python package for determining the coding-region consequences of a collection of variants.
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='veffects',
+     version='0.1',
+     description='Python package for determining the coding-region \
+     consequences of a collection of variants',
+     url='https://github.com/rrlove/veffects',
+     author='Becca Love',
+     author_email='rachelrebeccalove@gmail.com',
+     license='GNU General Public License v3.0',
+     packages=['veffects'],
+     zip_safe=False
+     )
